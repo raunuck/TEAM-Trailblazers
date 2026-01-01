c673db0338e6c2885f0c2ceaea4bb7205893e735
raunuck <raunak.satna@gmail.com>
2026-01-02 01:56:38 +0530

    connected supabase to the flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:adaptive_student_time/core/theme.dart';
 import 'package:adaptive_student_time/screens/auth/login_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://dgramltguzqwpxwwpdyw.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRncmFtbHRndXpxd3B4d3dwZHl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwOTExNzgsImV4cCI6MjA4MjY2NzE3OH0.UG09iFMcYbDgPSQ3kKB0T3X4EWJwS11DPjHsdt_W5uw',
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/services/supabase_service.dart
@@ -0,0 +1,42 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseService {
+  final SupabaseClient _client = Supabase.instance.client;
+
+  // We will add functions here
+  Future<List<Map<String, dynamic>>> fetchTasks() async {
+  final response = await _client
+      .from('tasks')
+      .select()
+      .order('created_at');
+
+  return List<Map<String, dynamic>>.from(response);
+}
+Future<void> createTask({
+  required String title,
+  int? estimatedMinutes,
+  String? priority,
+}) async {
+  final userId = _client.auth.currentUser!.id;
+
+  await _client.from('tasks').insert({
+    'user_id': userId,
+    'title': title,
+    'estimated_minutes': estimatedMinutes,
+    'priority': priority,
+  });
+}
+Future<void> completeTask(String taskId) async {
+  await _client
+      .from('tasks')
+      .update({'is_completed': true})
+      .eq('id', taskId);
+}
+Future<void> deleteTask(String taskId) async {
+  await _client
+      .from('tasks')
+      .delete()
+      .eq('id', taskId);
+}
+
+}
